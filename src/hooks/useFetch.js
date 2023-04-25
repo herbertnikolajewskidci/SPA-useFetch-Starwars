355f2378faab612892acb629e2bbc0eb2bb67f62
Herbert Nikolajewski <herbert.nikolajewksi@digitalcareerinstitute.org>
2023-04-25 15:08:07 +0200

    useCallback

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,16 +3,19 @@ import { useCallback, useEffect, useState } from "react";
 export default function useFetch(url) {
     const [characters, setCharacters] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const fetchData = async (url) => {
-        const res = await fetch(url);
-        const data = await res.json();
-        setCharacters(
-            data.results.map((char, index) => {
-                return { ...char, id: index + 1 };
-            })
-        );
-        setIsLoading(false);
-    };
+    const fetchData = useCallback(
+        async (url) => {
+            const res = await fetch(url);
+            const data = await res.json();
+            setCharacters(
+                data.results.map((char, index) => {
+                    return { ...char, id: index + 1 };
+                })
+            );
+            setIsLoading(false);
+        },
+        [url]
+    );
 
     useEffect(() => {
         fetchData(url);
