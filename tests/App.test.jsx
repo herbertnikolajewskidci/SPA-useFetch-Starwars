db80052704f048743f3d43c5a5ebf98bf08ea6be
Herbert Nikolajewski <herbert.nikolajewksi@digitalcareerinstitute.org>
2023-04-25 15:57:48 +0200

    code buddy

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,37 +1,14 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
-import App from "../src/App";
+import { renderHook } from "@testing-library/react-hooks";
+import useFetch from "../src/hooks/useFetch";
 
-describe("App", () => {
-    test("renders 404 page when invalid URL is entered", () => {
-        window.history.pushState({}, "Test page", "/invalid-url");
-        render(<App />);
-        expect(screen.getByText(/404/i)).toBeInTheDocument();
-    });
-
-    test("renders navbar on every page", () => {
-        render(<App />);
-        const navbar = screen.getByRole("navigation");
-        expect(navbar).toBeInTheDocument();
-    });
-
-    test("adds active class to selected link in navbar", () => {
-        render(<App />);
-        fireEvent.click(screen.getByText(/about/i));
-        const aboutLink = screen.getByText(/about/i);
-        expect(aboutLink).toHaveClass("active");
-
-        fireEvent.click(screen.getByText(/home/i));
-        const homeLink = screen.getByText(/home/i);
-        expect(homeLink).toHaveClass("active");
-    });
-
-    test("renders index page on first load", () => {
-        render(<App />);
-        expect(
-            screen.getByText(/startseite/i) ||
-                screen.getByText(/home/i) ||
-                screen.getByText(/page/i)
-        ).toBeInTheDocument();
+describe("useFetch", () => {
+    it("should return characters and isLoading", () => {
+        const url = "https://swapi.dev/api/people/";
+        const { result, waitForNextUpdate } = renderHook(() => useFetch(url));
+        setTimeout(async () => {
+            await waitForNextUpdate();
+            expect(result.current[0]).not.toHaveLength(0);
+            expect(result.current[1]).toBe(false);
+        }, 2000);
     });
 });
